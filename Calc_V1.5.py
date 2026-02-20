3e9ee2e4c4b659c0e89e194bcdb8245f59c13962
Yurt-the-coder <will.yurt08@gmail.com>
2026-02-20 13:02:15 -0500

    Update Calc_V1.5.py

--- Calc_V1.5.py
+++ Calc_V1.5.py
@@ -6,16 +6,24 @@ Will be implemented into Calc_V1.0 as V2.0
 
 import re
 
-txt_to_search = "34+1*5"
+txt_to_search = "34567890/60"
 
 operators_search = re.compile(r'[+\-*/%]')
-
 operators = operators_search.finditer(txt_to_search)
 
-num_search = re.compile(r'\d')
+
+num_search = re.compile(r'\d') #searches for single digits
+
+#num_search = re.compile(r'\d\d')  #searches for 2 numbers in a row
+#num_search = re.compile(r'\d\d\d')  #searches for 3 numbers in a row
+#num_search = re.compile(r'\d\d\d\d')  #searches for 4 numbers in a row
+#num_search = re.compile(r'\d\d\d\d\d')  #searches for 5 numbers in a row
+#num_search = re.compile(r'\d\d\d\d\d\d')  #searches for 6 numbers in a row
 
 numbers = num_search.finditer(txt_to_search)
 
+#code below shows that it is working to me, will be removed later
+#need to figure out how to index numbers and operators.
 print('\nOperators')
 for match in operators:
     print (match)
